3b8d710a89adb1d91869e5a5fd113f81727a4084
malwareslayer <dwi-mulia-mokoginta@protonmail.com>
2023-08-31 23:01:21 +0700

    Update amd installation

--- amd.sh
+++ amd.sh
@@ -8,4 +8,4 @@ sed -i 's/env = __GLX_VENDOR_LIBRARY_NAME,mesa/env = __GLX_VENDOR_LIBRARY_NAME,a
 sed -i 's/^# env = DISABLE_LAYER_AMD_SWITCHABLE_GRAPHICS_1,1/env = DISABLE_LAYER_AMD_SWITCHABLE_GRAPHICS_1,1/' .config/hypr/hyprland.conf
 sed -i 's/^# env = VK_ICD_FILENAMES,\/usr\/share\/vulkan\/icd.d\/amd_pro_icd64.json/env = VK_ICD_FILENAMES,\/usr\/share\/vulkan\/icd.d\/amd_pro_icd64.json/' .config/hypr/hyprland.conf
 
-cp -rf .config .local .gtkrc-2.0 "$HOME"/
+cp -rf .config/ .local/ .gtkrc-2.0 "$HOME"/
