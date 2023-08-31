fe2ea90aed59ad318a6cceb165bb7ddc901b960b
malwareslayer <dwi-mulia-mokoginta@protonmail.com>
2023-08-31 22:33:02 +0700

    Update amd installer

--- amd.sh
+++ amd.sh
@@ -5,7 +5,7 @@ sudo pacman -S sddm hyprland xdg-desktop-portal xdg-desktop-portal-hyprland xdg-
 sed -i 's/env = LIBVA_DRIVER_NAME,/env = LIBVA_DRIVER_NAME,radeonsi/' .config/hypr/hyprland.conf
 sed -i 's/env = VDPAU_DRIVER,/env = VDPAU_DRIVER,radeonsi/' .config/hypr/hyprland.conf
 sed -i 's/env = __GLX_VENDOR_LIBRARY_NAME,mesa/env = __GLX_VENDOR_LIBRARY_NAME,amd/' .config/hypr/hyprland.conf
-sed -i 's/^# env = DISABLE_LAYER_AMD_SWITCHABLE_GRAPHICS_1,1/ env = DISABLE_LAYER_AMD_SWITCHABLE_GRAPHICS_1,1/' .config/hypr/hyprland.conf
-sed -i 's/^# env = VK_ICD_FILENAMES,/usr/share/vulkan/icd.d/amd_pro_icd64.json/env = VK_ICD_FILENAMES,/usr/share/vulkan/icd.d/amd_pro_icd64.json/' .config/hypr/hyprland.conf
+sed -i 's/^# env = DISABLE_LAYER_AMD_SWITCHABLE_GRAPHICS_1,1/env = DISABLE_LAYER_AMD_SWITCHABLE_GRAPHICS_1,1/' .config/hypr/hyprland.conf
+sed -i 's/^# env = VK_ICD_FILENAMES,\/usr\/share\/vulkan\/icd.d\/amd_pro_icd64.json/env = VK_ICD_FILENAMES,\/usr\/share\/vulkan\/icd.d\/amd_pro_icd64.json/' .config/hypr/hyprland.conf
 
-cp -rf .config .local "$HOME"/
+cp -rf .config .local .gtkrc-2.0 "$HOME"/
